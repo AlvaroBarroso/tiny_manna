74a3ecfa147cd6542066befc12a0598f78f3fe09
Alvaro Barroso <alva15151515@gmail.com>
2023-06-03 11:59:01 -0300

    mover un poco de logica a la parte 3

--- remake/tp1/tiny_manna.cpp
+++ remake/tp1/tiny_manna.cpp
@@ -80,7 +80,7 @@ static void desestabilizacion_inicial(Manna_Array& h)
     for (int i = 0; i < N; ++i) {
         if (h[i] == 1) {
             h[i] = 0;
-            int j = i + 2 * (rand() % 2) - 1; // izquierda o derecha
+            int j = i + 2 * (rand() & 1) - 1; // izquierda o derecha
 
             // corrijo por condiciones periodicas
             if (j == N) {
@@ -123,10 +123,10 @@ static void descargar(Manna_Array& h, Manna_Array& dh)
         if (h[i] > 1) {
             for (int j = 0; j < h[i]; ++j) {
                 // sitio receptor a la izquierda o derecha teniendo en cuenta condiciones periodicas
-                int k = (i + 2 * (rand() % 2) - 1 + N) % N;
+                int k = (i + 2 * (rand() & 1) - 1 + N) % N;
+                
                 ++dh[k];
             }
-            h[i] = 0;
         }
     }
 
@@ -138,7 +138,7 @@ static void descargar(Manna_Array& h, Manna_Array& dh)
 #endif
     // PARTE 3
     for (int i = 0; i < N; ++i) {
-        h[i] += dh[i];
+        h[i] = dh[i] + (h[i] == 1 ? 1 : 0);
     }
 #ifdef PROFILE
     end = std::chrono::high_resolution_clock::now();
